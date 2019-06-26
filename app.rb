b617a4e3fa0809841d5f4fa179ce7c749f724259
Margot Konig <margot.konig@blackrock.com>
2019-06-26 20:18:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename
+  end
+  
+  get '/square/:number' do 
+    @num = params[:number].to_i 
+    erb :square 
+  end 
+  
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :
 end
